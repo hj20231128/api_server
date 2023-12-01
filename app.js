ec4a227d5092bcc31581f25e1d0b13044d283d98
awshj20231128 <aws.hj.20231128@gmail.com>
2023-12-01 10:41:44 +0900

    urlencoded

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
