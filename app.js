024817225005686c0e894edb93fab192155c94cf
awshj20231128 <aws.hj.20231128@gmail.com>
2023-12-01 10:12:59 +0900

    api2

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.set('port', process.env.PORT || 3000);
 //     console.error(err);
 //   });
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
